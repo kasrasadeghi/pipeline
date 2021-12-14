b558291deae90082e01546a0f89af064354436d5
kasrasadeghi <kasra.sadeghi1@gmail.com>
2021-12-13 19:19:13 -0600

    extract journal.py

--- main.py
+++ main.py
@@ -856,13 +856,6 @@ def creation():
                      colsfunc=lambda x: (FLAT.metadata(x)['Date'],),
                      namefunc=FLAT.title)
 
-@app.route("/journals")
-def journal_list():
-  return RENDER.LIST(reversed([x for x in FLAT.list_by_create_date() if 'Tags' in FLAT.metadata(x) and "Journal" in set(FLAT.metadata(x)['Tags'].split())]),
-                     title="Journals",
-                     linkfunc=FLAT.to_url,
-                     colsfunc=lambda x: (FLAT.metadata(x)['Date'],),
-                     namefunc=FLAT.title)
 
 @app.route("/tag/<tag>")
 def tag(tag):
@@ -899,16 +892,6 @@ def git_diff(filename):
 @app.route("/git/diff-staged/<path:filename>")
 def git_diff_staged(filename):
   return RENDER.GIT_DIFF(filename, staged=True)
-
-
-@app.route("/today")
-def today():
-  return redirect(FLAT.to_url(FLAT.to_disc(FLAT.today())), code=302)
-
-@app.route("/yesterday")
-def yesterday():
-  return redirect(FLAT.to_url(FLAT.to_disc(FLAT.yesterday())), code=302)
-
 
 @app.route("/note/<note>", methods=['GET', 'POST'])
 def get_note(note):
@@ -950,6 +933,7 @@ def route_edit(note):
 
   return RENDER.EDIT(note)
 
+kaz_import('journal.py')
 kaz_import('graph.py')
 kaz_import('rm.py')
 
