2c43b4ac80f0fcaf20af691147bef0148df64b9e
kasrasadeghi <kasra.sadeghi1@gmail.com>
2022-07-01 21:37:31 -0700

    extract util.py

--- main.py
+++ main.py
@@ -15,56 +15,9 @@ def kaz_import(filepath):
   with open(filepath) as f:
     exec(f.read(), globals())
 
-# LIB
-
-# this class is a namespace
-class util:
-  @classmethod
-  def sort_recent(_, files, root_path):
-    return [p[0] for p in sorted([(f, os.stat(root_path +'/'+ f).st_mtime) for f in files], key = lambda p: p[1])]
-
-  @classmethod
-  def basename(_, filename):
-    return filename.rsplit("/", 1)[1]
-
-  @classmethod
-  def get_current_time(_):
-    datecmd=["date", "+%a %b %e %T %Z %Y"]  # from emacs/lisp/kaz.el's kaz/current-time
-    return check_output(datecmd).decode('latin-1')
-
-  @classmethod
-  def parse_time_to_utc(_, time):
-    """
-    translated from date '+%a %b %e %T %Z %Y'
-    - used for comparisons and sorting
-    """
-    # convert the timezone to UTC because datetime in python doesn't handle timezones, omegalul
-    clean_time = check_output(["date", "-d", time, "-u", "+%a %b %d %T %Z %Y"]).decode('utf8').strip()
-    return datetime.datetime.strptime(clean_time, "%a %b %d %H:%M:%S %Z %Y")
-
-  @classmethod
-  def date_cmd(_, *l):
-    return check_output(["date", *l]).decode('utf8').strip()
-
-  @classmethod
-  def read_file(R, filepath):
-
-    if not os.path.isfile(filepath):
-      print(f"ERROR: reading file that does not exist: '{filepath}'")
-      return ""
-
-    acc = list()
-
-    with open(filepath) as f:
-      c = f.read()
-      if not c.endswith("\n"):
-        acc.append("\n")
-      acc.append(c)
-    return "".join(acc)
-
-# END LIB
-
 # IMPORTS
+
+kaz_import('util.py')
 
 kaz_import('flat.py')
 kaz_import('git.py')
