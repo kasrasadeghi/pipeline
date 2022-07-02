a62f57701637453066f07a4f0bc09c409b2909c4
kasrasadeghi <kasra.sadeghi1@gmail.com>
2022-07-01 22:53:00 -0700

    route /{after,before}/<note>

--- journal.py
+++ journal.py
@@ -11,6 +11,10 @@ class JOURNAL:
             "year": year,
             "title": title}
 
+  @staticmethod
+  def date_to_title(date_str):
+    return JOURNAL.date_to_parts(date_str)['title']
+
 @app.route("/journals")
 def journal_list():
   return FLAT_RENDER.LIST(reversed([x for x in FLAT.list_by_create_date() if 'Tags' in FLAT.metadata(x) and "Journal" in set(FLAT.metadata(x)['Tags'].split())]),
@@ -26,3 +30,25 @@ def today():
 @app.route("/yesterday")
 def yesterday():
   return redirect(FLAT.to_url(FLAT.to_disc(FLAT.yesterday())), code=302)
+
+@app.route("/before/<note>")
+def route_before(note):
+  day_before_title = JOURNAL.date_to_title(util.day_before(FLAT.metadata(note)['Date']))
+
+  # look for a note
+  for n in FLAT.list():
+    if day_before_title == FLAT.title(n):
+      return redirect(FLAT.to_url(FLAT.to_disc(n)))
+
+  return "ERROR: could not find note before"
+
+@app.route("/after/<note>")
+def route_after(note):
+  day_after_title = JOURNAL.date_to_title(util.day_after(FLAT.metadata(note)['Date']))
+
+  # look for a note
+  for n in FLAT.list():
+    if day_after_title == FLAT.title(n):
+      return redirect(FLAT.to_url(FLAT.to_disc(n)))
+
+  return "ERROR: could not find note after"
