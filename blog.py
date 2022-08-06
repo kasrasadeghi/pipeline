407496f9128158069812aa6e2fa3dca5120c3974
kasrasadeghi <kasra.sadeghi1@gmail.com>
2022-08-06 15:14:09 -0700

    basic blog rendering

--- blog.py
+++ blog.py
@@ -2,40 +2,83 @@ class BLOG:
   root = "37a95556-2ec0-4f0d-b6af-d3dd263d4763.note"
 
   @staticmethod
-  def parse():
-    with FLAT.open_note(BLOG.root) as N:
-      # for some reason, "f.readlines()" and "for L in f:" don't usually do what
-      # i expect so i'm using splitlines().  I think splitlines handles weird
-      # cases with empty last lines and empty front matter correctly, actually
-      # preserving them instead of randomly chopping them off
-      lines = N.read().splitlines()
+  def title():
+    lines = FLAT.note_read_lines(BLOG.root)
 
-    line_iter = iter(enumerate(lines))
-    acc = []
-    line_i = 0
-    def get():
-      nonlocal line_i
-      line_i, line = next(line_iter)
-      return line
+    if len(lines) == 0:
+      ABORT(f"ERROR: blog root file is empty {BLOG.root}")
 
-    def abort(line):
-      return f"ERROR: on line '{i}' '{line}'"
-
-    first_line = get()
+    # parse title
+    first_line = lines[0]
     if not first_line.startswith("# "):
-      return abort(first_line)
-
+      ABORT(f"ERROR: parsing title on first line: '{lines[0]}'")
     title = first_line.removeprefix("# ")
 
-    for i, l in line_iter:
-      if l == "--- METADATA ---":
-        break
-      acc.append(l)
-    return title, "\n".join(acc)
+    return title
 
+  @staticmethod
+  def parse():
+    lines = FLAT.note_read_lines(BLOG.root)
+
+    node = PARSER.parse_content("\n".join(lines[1:]))
+
+    if node[0]['section'] != "entry":
+      ABORT(f"ERROR: parsing entry section from node: {TREE.dump_tree(node)}")
+
+    blocks = list(filter(lambda x: not TREE.is_newline(x), node[0]['blocks']))
+
+    for b in blocks:
+      if not TREE.is_singleton(b):
+        ABORT("ERROR: parsing blog post")
+      blog_post = b[0]
+      LOG(blog_post)
+      internal_name = blog_post['value']
+      title, filename, note = [c['value'] for c in blog_post['children']]
+      title = title.removeprefix("title: ")
+      filename = "blog/posts/" + filename.removeprefix("filename: ")
+      note = note.removeprefix("note: ")
+
+      yield DICT(internal_name, title, filename, note)
+
+  @staticmethod
+  def post(post):
+    return f'<a href="{post.filename}">{post.title}</a>'
+
+  @staticmethod
+  def bar():
+    note = BLOG.root
+    return FLAT_RENDER._bar(
+      note,
+      f'<a href="/note/{note}">note</a>'
+      f'<a href="/edit/{note}">edit</a>'
+    )
+
+  @staticmethod
+  def RENDER():
+    bar = BLOG.bar()
+
+    try:
+      acc = []
+      for blog_post in BLOG.parse():
+        acc.append("<li>" + BLOG.post(blog_post) + "</li>")
+      content = "<ul>blog post list:" + "\n".join(acc) + "</ul>"
+
+      title = BLOG.title()
+    except Boundary as e:
+      content = "couldn't parse blog list:\n" + DEBUG.CATCH(e)
+      title = "Kaz Blog"
+
+    # compose html
+
+    result = (
+      f"<div class=\"msgbox\" style='font-feature-settings: \"liga\" 0'>"
+      f"{content}</div>"
+      f'<form method="post"><input class="msg_input" autocomplete="off" autofocus type="text" name="msg"></form>'
+    )
+    return RENDER.base_page(DICT(title, content, bar))
 
 
 @app.route("/blog")
 def get_blog():
-  title, content = BLOG.parse()
-  return DEBUG.TEXT(title, content)
+  DEBUG.init_state()
+  return BLOG.RENDER()
