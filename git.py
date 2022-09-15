d49c8fa8201d87f85348507d2d6eb55149697496
kasrasadeghi <kasra.sadeghi1@gmail.com>
2022-09-14 18:30:36 -0700

    clean up git a lot by using cwd= arg for subprocess

--- git.py
+++ git.py
@@ -1,52 +1,35 @@
 class GIT_HANDLE:
   @classmethod
   def _add(cls, note):
-    currdir = os.getcwd()
-    os.chdir(FLAT.path)
-
     if FLAT.exists(note):
-      check_output(['git', 'add', note]).decode('utf8').strip()
-
-      os.chdir(currdir)
+      check_output(['git', 'add', note]).strip()
       return redirect(f"/git/diff-staged/{note}", code=302)
 
     else:
       LOG(f"ERROR: note '{note}' doesn't exist to 'git add'")
-      os.chdir(currdir)
       return Response('', 204)
 
   @classmethod
   def _unstage(cls, note):
-    currdir = os.getcwd()
-    os.chdir(FLAT.path)
-
     if FLAT.exists(note):
-      check_output(['git', 'restore', '--staged', note]).decode('utf8').strip()
-
-      os.chdir(currdir)
-      return redirect(f"/git/diff/{note}", code=302)
+      check_output(['git', 'restore', '--staged', note]).strip()
+      return redirect(f"/git/diff/{note}")
 
     else:
       LOG(f"ERROR: note '{note}' doesn't exist to 'git add'")
-      os.chdir(currdir)
       return Response('', 204)
 
   @classmethod
   def _commit(cls, message):
-    currdir = os.getcwd()
-    os.chdir(FLAT.path)
-
-    stage_size = len(check_output(['git', 'diff', '--staged']).decode('utf8').strip())
+    stage_size = len(FLAT.cmd(['git', 'diff', '--staged']).strip())
 
     # if we have something staged, commit it and redirect to showing that commit
     if stage_size != 0:
-      check_output(['git', 'commit', '-m', message]).decode('utf8').strip()
-      just_committed = check_output(['git', 'rev-parse', 'HEAD']).decode('utf8').strip()
-      os.chdir(currdir)
+      FLAT.cmd(['git', 'commit', '-m', message]).strip()
+      just_committed = FLAT.cmd(['git', 'rev-parse', 'HEAD']).strip()
       return redirect("/git/show/" + just_committed)
 
     # if we have nothing staged, then show the log
-    os.chdir(currdir)
     return redirect("/git/log")
 
 # END HANDLE
@@ -55,7 +38,7 @@ class GIT_HANDLE:
 class GIT:
   @classmethod
   def _git_porcelain(R):
-    status = check_output(['git', 'status', '--porcelain']).decode('utf-8')  # NO .strip() !
+    status = FLAT.cmd(['git', 'status', '--porcelain'])  # NO .strip() !
 
     acc = list()
 
@@ -110,7 +93,7 @@ class GIT:
 
   @classmethod
   def _git_status(R):
-    status = check_output(['git', 'status']).decode('utf8').strip()
+    status = FLAT.cmd(['git', 'status']).strip()
 
     UUID_LEN = len("2e62fd14-9b3a-4275-9a39-7bf3d6b488c7.note")
 
@@ -128,14 +111,14 @@ class GIT:
   @classmethod
   def _git_diff_single(R, note):
     """the diff between working and stage"""
-    diff = check_output(['git', '-c', 'color.ui=always', 'diff', note]).decode('utf8').strip()
+    diff = FLAT.cmd(['git', '-c', 'color.ui=always', 'diff', note]).strip()
     diff = RENDER_UTIL._parse_color(str(escape(diff)))
     return diff
 
   @classmethod
   def _git_diff_staged(R, note):
     """the diff between stage and head"""
-    diff = check_output(['git', '-c', 'color.ui=always', 'diff', '--staged', note]).decode('utf8').strip()
+    diff = FLAT.cmd(['git', '-c', 'color.ui=always', 'diff', '--staged', note]).strip()
     diff = RENDER_UTIL._parse_color(str(escape(diff)))
     return diff
 
@@ -143,20 +126,20 @@ class GIT:
   def _git_diff(R):
     """the diff between work and stage"""
     # git color always: https://stackoverflow.com/questions/16073708/force-git-status-to-output-color-on-the-terminal-inside-a-script
-    diff = check_output(['git', '-c', 'color.ui=always', 'diff']).decode('utf8').strip()
+    diff = FLAT.cmd(['git', '-c', 'color.ui=always', 'diff']).strip()
     diff = RENDER_UTIL._parse_color(str(escape(diff)))
     return diff
 
   @classmethod
   def _git_stage(R):
     """show the stage as the diff between it and HEAD"""
-    diff = check_output(['git', '-c', 'color.ui=always', 'diff', '--staged']).decode('utf8').strip()
+    diff = FLAT.cmd(['git', '-c', 'color.ui=always', 'diff', '--staged']).strip()
     diff = RENDER_UTIL._parse_color(str(escape(diff)))
     return diff
 
   @classmethod
   def _git_log(R):
-    diff = check_output(['git', '-c', 'color.ui=always', '--no-pager', 'log', '--decorate=short']).decode('utf8')
+    diff = FLAT.cmd(['git', '-c', 'color.ui=always', '--no-pager', 'log', '--decorate=short'])
 
     # add links for every commit
     prefix = RENDER_UTIL.ANSI() + "33m" + "commit "
@@ -180,18 +163,13 @@ class GIT:
 
   @classmethod
   def _git_show_commit(R, sha):
-    output = check_output(['git', '-c', 'color.ui=always', 'show', sha]).decode('utf8')
+    output = FLAT.cmd(['git', '-c', 'color.ui=always', 'show', sha])
     output = RENDER_UTIL._parse_color(str(escape(output)))
     return output
 
   @classmethod
   def _git_menu(R):
-
-    currdir = os.getcwd()
-    os.chdir(FLAT.path)
     menu = R._git_porcelain()
-    os.chdir(currdir)
-
     return f'<pre><h1>$ <a href="/git/menu">git status --porcelain</a></h1>{menu}</pre>'
 
   @classmethod
@@ -203,12 +181,9 @@ class GIT:
     title = "Git Status"
     header = f"<!DOCTYPE html><html><head>{RENDER_UTIL.STYLE()}<title>{title}</title></head><body><div class=\"content\">"
 
-    currdir = os.getcwd()
-    os.chdir(FLAT.path)
     status = R._git_status()
     diff = R._git_diff()
-    untracked_filenames = check_output(["git", "ls-files", "--others", "--exclude-standard"]).decode('utf8').strip()
-    os.chdir(currdir)
+    untracked_filenames = FLAT.cmd(["git", "ls-files", "--others", "--exclude-standard"]).strip()
 
     untracked_files = list()
     for l in untracked_filenames.splitlines():
@@ -249,13 +224,10 @@ class GIT:
     title = "Git Diff: " + filename_title
     header = f"<!DOCTYPE html><html><head>{RENDER_UTIL.STYLE()}<title>{title}</title></head><body><div class=\"content\">"
 
-    currdir = os.getcwd()
-    os.chdir(FLAT.path)
     if staged:
       output = R._git_diff_staged(filename)
     else:
       output = R._git_diff_single(filename)
-    os.chdir(currdir)
 
     content = (
       R._git_menu() +
@@ -270,11 +242,7 @@ class GIT:
     title = "Git Stage"
     header = f"<!DOCTYPE html><html><head>{RENDER_UTIL.STYLE()}<title>{title}</title></head><body><div class=\"content\">"
 
-    currdir = os.getcwd()
-    os.chdir(FLAT.path)
-
     output = R._git_stage()
-    os.chdir(currdir)
 
     content = (
       R._git_menu() +
@@ -290,10 +258,7 @@ class GIT:
     title = "Git Commit"
     header = f"<!DOCTYPE html><html><head>{RENDER_UTIL.STYLE()}<title>{title}</title></head><body><div class=\"content\">"
 
-    currdir = os.getcwd()
-    os.chdir(FLAT.path)
     output = R._git_stage()
-    os.chdir(currdir)
 
     content = (
       f'<pre><h1>return to <a href="/git/menu">git menu</a></h1></pre>' +
@@ -309,10 +274,7 @@ class GIT:
     title = "Git Commit"
     header = f"<!DOCTYPE html><html><head>{RENDER_UTIL.STYLE()}<title>{title}</title></head><body><div class=\"content\">"
 
-    currdir = os.getcwd()
-    os.chdir(FLAT.path)
     output = R._git_show_commit(sha)
-    os.chdir(currdir)
 
     content = (
       f'<pre><h1>return to <a href="/git/menu">git menu</a></h1></pre>' +
@@ -326,10 +288,7 @@ class GIT:
     title = "Git Log"
     header = f"<!DOCTYPE html><html><head>{RENDER_UTIL.STYLE()}<title>{title}</title></head><body><div class=\"content\">"
 
-    currdir = os.getcwd()
-    os.chdir(FLAT.path)
     log = R._git_log()
-    os.chdir(currdir)
 
     content = (
       R._git_menu() +
