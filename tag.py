86346141704f8fcabceac329f9d92caa9bced748
kasrasadeghi <kasra.sadeghi1@gmail.com>
2023-01-07 16:39:08 -0800

    ideas on improving tag parsing

--- tag.py
+++ tag.py
@@ -1,4 +1,6 @@
 class TAG:
+
+  # TODO improve tag parsing to be "any sequence of capitalized letters".  maybe 2 or more, maybe 3 or more?
   def parse(msg_content):
     words = msg_content.split()
     acc = list()
