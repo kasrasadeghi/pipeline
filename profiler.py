d7ddeff21c083da39d4da116097609b4fd943e70
kasrasadeghi <kasra.sadeghi1@gmail.com>
2023-02-16 11:56:05 -0800

    upgrade profiler: prefix selection, average time of last 10 requests

--- profiler.py
+++ profiler.py
@@ -8,10 +8,18 @@ def get_profile_targets():
   dumps.sort(key=get_timestamp)
   dumps.reverse()
 
+  prefix = request.args.get('prefix', default='')
+
   acc = []
+  duration_acc = []
   for filename in dumps:
+    if not filename.startswith(prefix):
+      continue
+    ms = filename.rsplit('.', 3)[1].removesuffix('ms') # get the milliseconds duration
+    duration_acc.append(int(ms))
+    LOG({'filename': filename})
     acc.append(f"     <option value='{filename}'>{get_timestamp(filename)}: {filename}</option>")
-  return "\n".join(acc)
+  return {'options': "\n".join(acc), 'average': sum(duration_acc[:10]) / len(duration_acc[:10])}
 
 @app.route('/api/profile-dump/<dump>')
 def get_profile_target(dump):
@@ -22,18 +30,23 @@ def get_profile_target(dump):
   outputstream = io.StringIO()
   import pstats
   p = pstats.Stats(filename, stream=outputstream)
-  # p.strip_dirs()
-  p.sort_stats('cumulative')
+  p.sort_stats('tottime')
   p.print_stats()
+  p.sort_stats('cumulative')
   p.print_callers()
   p.print_callees()
-  return outputstream.getvalue()
+  return FLASK_UTIL.ESCAPE(outputstream.getvalue())
 
 @app.route('/test/profile')
 def get_profile_data_viewer():
   # ideas:
   content = (
     """<script>
+      document.addEventListener('load', () => {
+        let url = new URL(window.location);
+        url.searchParams.set('profile');
+        document.getElementById('select-profile').value = url.href;
+      });
       function get_profile() {
         const path = document.getElementById('select-profile').value;
         console.log('getting profile dump ', path);
@@ -53,10 +66,14 @@ def get_profile_data_viewer():
         event.preventDefault();
       }
       function refresh_profiles() {
-        fetch('/api/profile-dumps')
-          .then((res) => res.text())
+        let url = new URL('/api/profile-dumps', window.location.origin);
+        const prefix = document.getElementById('prefix').value;
+        url.searchParams.set('prefix', prefix);
+        fetch(url.href)
+          .then((res) => res.json())
           .then((data) => {
-             document.getElementById('select-profile').innerHTML = data;
+             document.getElementById('select-profile').innerHTML = data['options'];
+             document.getElementById('profile-average').innerHTML = data['average'];
           })
           .catch((err) => {
              document.getElementById('content-result').innerHTML = err;
@@ -71,13 +88,18 @@ def get_profile_data_viewer():
     </script>"""
     "<form>"
       "<select id='select-profile' name='profile-target'>"
-      + get_profile_targets() + \
+      + get_profile_targets()['options'] + \
       "</select>"
       "<br/>"
       "<input type=button value='get profile' onclick='get_profile()'></input>"
+    "</form>"
+    "<form>"
+      "<label for='prefix' value='filter prefix'></label>"
+      "<input type=text value='' id='prefix' onsubmit='refresh_profiles()()'></input>"
       "<input type=button value='refresh profile targets' onclick='refresh_profiles()'></input>"
     "</form>"
     "<p id='profile-requested'>no result yet<p>"
+    "<p id='profile-average'>no average time yet<p>"
     "<pre id='content-result'></pre>"
   )
   return RENDER.base_page({'title': 'Profiler Output', 'content': content})
