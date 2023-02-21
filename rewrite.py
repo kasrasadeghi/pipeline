f9f1c2c3268607525183a0e11650fca97983f7c6
kasrasadeghi <kasra.sadeghi1@gmail.com>
2023-02-21 13:43:40 -0800

    rewrite: message idiom

--- rewrite.py
+++ rewrite.py
@@ -1,7 +1,23 @@
 class REWRITE:
   @staticmethod
+  def block(block, **kwargs):
+    if DISCUSSION.block_is_msg(block):
+      return {'date': DISCUSSION.date(block[0]), 'msg': DISCUSSION.msg_content(block[0])}
+    return block
+
+  @staticmethod
+  def section(section, **kwargs):
+    acc = {'blocks': list()}
+    for block in section['blocks']:
+      acc['blocks'].append(REWRITE.block(block, **kwargs))
+    return acc
+
+  @staticmethod
   def page(page, **kwargs):
-    return page
+    acc = list()
+    for section in page:
+      acc.append(REWRITE.section(section, **kwargs))
+    return acc
 
 @app.route('/api/parse/<note>')
 def api_parse(note):
