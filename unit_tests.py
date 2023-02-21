1717abf1af64554024628927ada4e1b8c6e8e3ef
kasrasadeghi <kasra.sadeghi1@gmail.com>
2023-02-21 10:55:37 -0800

    clear and save profiles, along with unit test: render every note

--- unit_tests.py
+++ unit_tests.py
@@ -23,3 +23,17 @@ def test_date_cmd():
   for i in range(10000):
     util.date_cmd("-d", 'Tue Feb 14 11:13:50 PST 2023', "+%T")
   return 'DONE'
+
+
+@app.route('/test/unit/render_every_note')
+def test_render_every_note():
+  for note in FLAT.list():
+    DISCUSSION_RENDER.MAIN(note)
+  return 'DONE'
+
+@app.route('/test/unit/pagesize_every_note')
+def test_pagesize_every_note():
+  size = 0
+  for note in FLAT.list():
+    size += len(DISCUSSION_RENDER.MAIN(note))
+  return 'rendered size: ' + str(size)
