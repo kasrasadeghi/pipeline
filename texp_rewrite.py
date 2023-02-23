aa109bdc3b6499f989378f949bcb84adb088367b
kasrasadeghi <kasra.sadeghi1@gmail.com>
2023-02-22 18:18:42 -0800

    rewrite: remove msg: and Date: prefixes from messages

--- texp_rewrite.py
+++ texp_rewrite.py
@@ -32,8 +32,10 @@ class TEXP_REWRITE:
   def tree(tree):
     """ rewrite: block -> (| message block)"""
     if (x := tree.match("(tree (node (indent 0) (value {content}) (children (node (indent 1) (value {date}) children)))))"))[0]:
-      msg = x[1]
+      msg = Texp('msg')
       msg.value = 'msg'
+      msg.push(Texp('content', x[1].at('content').removeprefix('msg: ')))
+      msg.push(Texp('Date', x[1].at('date').removeprefix('Date: ')))
       return msg
     return tree
 
