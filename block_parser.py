f11bbf3fbcab59b1215a5092523f732855abdf4f
kasrasadeghi <kasra.sadeghi1@gmail.com>
2023-02-24 19:22:35 -0800

    cleanup block_parser, 200ms -> 70ms

--- block_parser.py
+++ block_parser.py
@@ -23,63 +23,55 @@ class PARSER:
 
   @staticmethod
   def parse_content(content, **kwargs):
-    # EXPL: a file is a list of sections, which each have a title and a list of blocks
+    # EXPL: a page is a list of sections, which each have a title and a list of blocks
     # - a block is a list of nodes
     # - a node can be either a line of type 'str', or a parsed tree
-    R = LineReader(content)
-
     make_section = lambda title: {"title": title, "lines": list()}
 
     sections = list()
     curr_section = make_section("entry")
 
-    while R.hasNext():
-      i, L = R.get()
-      if L.startswith("--- ") and L.endswith(" ---"):
+    for line in content.split('\n'):
+      if line.startswith("--- ") and line.endswith(" ---"):
         # end previous section
         sections.append(curr_section)
 
-        title = L[len("--- "):-len(" ---")]
-        if not all(map(lambda c: c in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ ', title)):
+        title = line[len("--- "):-len(" ---")]
+        is_capital = lambda c: 0 <= ord(c) - ord('A') < 26
+        if not all(map(lambda c: c == ' ' or is_capital(c), title)):
           LOG(f"WARNING: invalid section title '{title}'")
         curr_section = make_section(title)
       else:
-        curr_section['lines'].append(L)
+        curr_section['lines'].append(line)
     sections.append(curr_section)
 
     for S in sections:
-      S['blocks'] = PARSER.parse_section("\n".join(S['lines']), **kwargs)
+      S['blocks'] = PARSER.parse_section(S['lines'], **kwargs)
 
     return sections
 
   @staticmethod
-  def parse_section(section, **kwargs):
-    R = LineReader(section)
-    # EXPL: a section is a list of blocks, which are each a list of lines
+  def parse_section(lines, **kwargs):
+    """turns a list of lines into a list of blocks"""
+
+    tree_parser = kwargs.get('tree_parser', TREE_PARSER.process_block)
+
     blocks = []
     curr_block = []
-    while R.hasNext():
-      i, l = R.get()
-
-      if "" == l:
+    for line in lines:
+      if "" == line:
         if len(curr_block):
-          blocks.append(curr_block)
+          blocks.append(tree_parser(curr_block, **kwargs))
         blocks.append([''])
         curr_block = []
       else:
-        curr_block.append(l)
+        curr_block.append(line)
 
     # avoids empty blocks at the end of sections
     if len(curr_block):
-      blocks.append(curr_block)
+      blocks.append(tree_parser(curr_block, **kwargs))
 
-    new_blocks = []
-    for B in blocks:
-      if 'tree_parser' in kwargs:
-        new_blocks.append(kwargs['tree_parser'](B, **kwargs))
-      else:
-        new_blocks.append(TREE_PARSER.process_block(B, **kwargs))
-    return new_blocks
+    return blocks
 
 @app.route('/parse/<note>')
 @app.route('/test/parse/<note>')
