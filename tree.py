aa993cb33c6a52448ca8ffddc3e6563627a13e08
kasrasadeghi <kasra.sadeghi1@gmail.com>
2023-02-25 11:24:58 -0800

    remove some now-unused rewrite-y things from TREE

--- tree.py
+++ tree.py
@@ -6,10 +6,6 @@ class TREE:
     return f"<pre><br/>(file size: {os.path.getsize(FLAT.to_path(note))} bytes)</pre>"
 
   @staticmethod
-  def blocks_from_section(section):
-    return TREE.squash_messages(TREE.trim_newlines(section['blocks']))
-
-  @staticmethod
   def is_newline(block):
     """ a newline block looks like this: [''] """
     return len(block) == 1 and block[0] == ''
@@ -23,54 +19,6 @@ class TREE:
   def is_line(block):
     """ a line is a block with one item that is a string, not a parse tree item """
     return len(block) == 1 and isinstance(block[0], str)
-
-  @staticmethod
-  def trim_newlines(section):
-    """ this function deletes consecutive newline blocks """
-    new_section = []
-
-    i = 0
-    while i < len(section):
-      block = section[i]
-      new_section.append(block)
-      debug("trim newlines", repr(block), TREE.is_newline(block))
-
-      if i < len(section) and TREE.is_newline(section[i]):
-        while i < len(section) and TREE.is_newline(section[i]):
-          i += 1
-          debug("trim newlines, consuming:", repr(block))
-      else:
-        i += 1
-
-      if i < len(section):
-        debug("next section:", section[i])
-
-    return new_section
-
-  @staticmethod
-  def squash_messages(blocks):
-    """ removes newline blocks between messages """
-    new_blocks = list()
-
-    i = 0
-    while i < len(blocks):
-      if TREE.is_newline(blocks[i]):
-        after_msg = (i-1) >= 0 \
-          and TREE.is_singleton(blocks[i-1]) \
-          and DISCUSSION.is_msg(blocks[i-1][0])
-
-        before_msg = (i+1) < len(blocks) \
-          and TREE.is_singleton(blocks[i+1]) \
-          and DISCUSSION.is_msg(blocks[i+1][0])
-
-        if after_msg and before_msg:
-          i += 1
-          continue
-
-      new_blocks.append(blocks[i])
-      i += 1
-
-    return new_blocks
 
   @staticmethod
   def dump_tree(page):
@@ -87,31 +35,3 @@ class TREE:
             acc.append(' ' + FLASK_UTIL.ESCAPE(EL))
           acc.append(']')
     return "\n".join(acc)
-
-@app.route('/render/trim_newline/<note>')
-def test_render_trim_newline(note):
-
-  page = PARSER.parse_file(FLAT.to_path(note))
-
-  new_page = list()
-  for S in page:
-    new_S = dict()
-    new_S['title'] = S['title']
-    new_S['blocks'] = TREE.trim_newlines(S['blocks'])
-    new_page.append(new_S)
-
-  return DEBUG.TEXT('test_parse', TREE.dump_tree(new_page))
-
-@app.route('/render/trim_and_squash/<note>')
-def test_render_trim_and_squash(note):
-
-  page = PARSER.parse_file(FLAT.to_path(note))
-
-  new_page = list()
-  for S in page:
-    new_S = dict()
-    new_S['title'] = S['title']
-    new_S['blocks'] = TREE.squash_messages(TREE.trim_newlines(S['blocks']))
-    new_page.append(new_S)
-
-  return DEBUG.TEXT('test_parse', TREE.dump_tree(new_page))
