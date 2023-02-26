2947452ef24787174f7b48843d3905b6e1f60228
kasrasadeghi <kasra.sadeghi1@gmail.com>
2023-02-25 17:25:53 -0800

    note should endswith('.note')

--- flat.py
+++ flat.py
@@ -46,7 +46,7 @@ class FLAT:
 
   @classmethod
   def list(cls):
-    return [x for x in os.listdir(cls.path) if cls.exists(x) and not (x[0] == '#' and x[-1] == '#')]
+    return [x for x in os.listdir(cls.path) if cls.exists(x) and x.endswith('.note') and not (x[0] == '#' and x[-1] == '#')]
 
   @staticmethod
   def list_by_mtime():
