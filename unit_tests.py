2f8ca5cd49bfe676ee2ada6757f766d9b52a8a55
kasrasadeghi <kasra.sadeghi1@gmail.com>
2023-03-26 20:36:05 -0700

    unit test: size of caching all of the notes

--- unit_tests.py
+++ unit_tests.py
@@ -60,3 +60,37 @@ def test_tag_parse():
       " testing I am testing 1 2 3"],
      "capital 'I' should not disrupt parsing non-tags"]
   )
+
+def total_size(o):
+  # from: https://code.activestate.com/recipes/577504/
+  from itertools import chain
+  dict_handler = lambda d: chain.from_iterable(d.items())
+  handlers = {tuple: iter,
+              list: iter,
+              dict: dict_handler,
+              set: iter,
+              frozenset: iter
+              }
+  seen = set()              # track which object id's have already been seen
+
+  def sizeof(o):
+    if id(o) in seen:       # do not double count the same object
+      return 0
+    seen.add(id(o))
+    s = sys.getsizeof(o)
+    if type(o) in handlers:
+      try:
+        s += sum(map(sizeof, handlers[type(o)](o)))  # get size of the objects it refers to as well
+      except:
+        import pdb
+        pdb.set_trace()
+    return s
+
+  return sizeof(o)
+
+@app.route('/test/unit/parse_all_memory')
+def test_parse_all_memory():
+  acc = list()
+  for note in FLAT.list():
+    acc.append(REWRITE.note(note))
+  return str(total_size(acc))
