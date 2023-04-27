b87cb0e956341d1d6e5e8c4aa64b6f0dd046cd8f
kasrasadeghi <kasra.sadeghi1@gmail.com>
2023-04-26 19:38:04 -0700

    remove unused method: TREE.is_line

--- tree.py
+++ tree.py
@@ -11,11 +11,6 @@ class TREE:
     return len(block) == 0
 
   @staticmethod
-  def is_line(block):
-    """ a line is a block with one item that is a string, not a parse tree item """
-    return len(block) == 1 and isinstance(block[0], str)
-
-  @staticmethod
   def dump_tree(page):
     acc = list()
     for S in page:
