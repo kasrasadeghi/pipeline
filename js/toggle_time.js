c0405356b5af37b4c860d5ed541d3e40ffb8cfc8
kasrasadeghi <kasra.sadeghi1@gmail.com>
2023-05-27 20:05:55 -0700

    partially working localStorage string fix

--- js/toggle_time.js
+++ js/toggle_time.js
@@ -1,18 +1,23 @@
-let time_displayed = true;
-
-if (window.localStorage.getItem('time_displayed') !== undefined) {
-  time_displayed = window.localStorage.getItem('time_displayed');
+console.log("local storage for 'time_displayed'", window.localStorage.getItem('time_displayed'));
+if (window.localStorage.getItem('time_displayed') === undefined) {
+  window.localStorage.setItem('time_displayed', true);
 }
 
-function toggle_time() {
+function render_time() {
   let timestamps = document.getElementsByClassName("msg_timestamp");
-  time_displayed = !time_displayed;
-  window.localStorage.setItem('time_displayed', time_displayed);
   for (let timestamp of timestamps) {
-    if (time_displayed) {
+    if (window.localStorage.getItem('time_displayed') === 'true') {
       timestamp.style.display = '';
     } else {
       timestamp.style.display = 'none';
     }
   }
 }
+
+function toggle_time() {
+  window.localStorage.setItem('time_displayed', window.localStorage.getItem('time_displayed') === 'false');
+  console.log("local storage for 'time_displayed'", window.localStorage.getItem('time_displayed'));
+  render_time();
+}
+
+window.addEventListener('load', render_time);
