fbe5ae4ac82ad77d93a46d5bf9b69140487a0ec3
kasrasadeghi <kasra.sadeghi1@gmail.com>
2023-05-27 19:43:53 -0700

    time_displayed localStorage

--- js/toggle_time.js
+++ js/toggle_time.js
@@ -1,8 +1,13 @@
 let time_displayed = true;
 
+if (window.localStorage.getItem('time_displayed') !== undefined) {
+  time_displayed = window.localStorage.getItem('time_displayed');
+}
+
 function toggle_time() {
   let timestamps = document.getElementsByClassName("msg_timestamp");
   time_displayed = !time_displayed;
+  window.localStorage.setItem('time_displayed', time_displayed);
   for (let timestamp of timestamps) {
     if (time_displayed) {
       timestamp.style.display = '';
