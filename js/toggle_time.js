6985c027c010f5f883542c31ce9d4f84e8289ef7
kasrasadeghi <kasra.sadeghi1@gmail.com>
2023-06-06 18:04:02 -0700

    partial work on init_timestamp startup shake.
    - proper fix might be to include source straight into the html

--- js/toggle_time.js
+++ js/toggle_time.js
@@ -3,6 +3,19 @@ if (window.localStorage.getItem('time_displayed') === undefined) {
   window.localStorage.setItem('time_displayed', true);
 }
 
+
+let init_time_displayed = window.localStorage.getItem('time_displayed') === 'true';
+function init_time_display() {
+  let timestamps = document.getElementsByClassName("msg_timestamp");
+  for (let timestamp of timestamps) {
+    if (init_time_displayed) {
+      timestamp.style.display = '';
+    } else {
+      timestamp.style.display = 'none';
+    }
+  }
+}
+
 function render_time() {
   let timestamps = document.getElementsByClassName("msg_timestamp");
   for (let timestamp of timestamps) {
@@ -19,5 +32,3 @@ function toggle_time() {
   console.log("local storage for 'time_displayed'", window.localStorage.getItem('time_displayed'));
   render_time();
 }
-
-window.addEventListener('load', render_time);
