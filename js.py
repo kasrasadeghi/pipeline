87ad82abbb5de40a4b92cd9461b6f34cb3626fcb
kasrasadeghi <kasra.sadeghi1@gmail.com>
2023-09-27 21:37:14 -0700

    custom include and link point in order to standardize include logic
    allows for no extra calls to backend for loading js

--- js.py
+++ js.py
@@ -2,3 +2,11 @@
 def get_js(path):
   from flask import send_from_directory
   return send_from_directory('js', path)
+
+def link_js(path):
+  return f"<script src='/js/{path}'></script>"
+
+def include_js(path):
+  with open('js/' + path) as f:
+    content = f.read()
+  return f"<script>" + content + "</script>"
